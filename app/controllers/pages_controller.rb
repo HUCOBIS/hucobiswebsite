cb85c58ccfcf5f12e3bdae73435a2e00ce996e11
Zuri Hunter <zurihunter92@gmail.com>
2015-01-16 23:48:25 +0000

    Assigment of Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+  def home
+  end
+  
+  def about
+  end
+  
+  def projects
+  end
+  
+  def blog
+  end
+  
+  def events
+  end
+  
+  def programs
+  end
+  
+  def comeup
+  end
+  
+  def contact
+  end
+  
+end
